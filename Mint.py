a9e5e7f1a2a7dca387a87a49973360ccc470220a
DSandberg93 <Danisan@stud.ntnu.no>
2017-02-21 12:34:50 +0100

    Add files via upload

---
+++ Mint.py
@@ -0,0 +1,40 @@
+import pymysql.cursors
+import pymysql
+
+class Mint():
+
+
+    def connect(self, host=str, user=str, password=str, db=str):
+        connection = pymysql.connect(host=host, user=user, password=password, db=db, charset='utf8')
+        return connection
+
+    def add_keyword(self, word=str, priority=str, idinformation=str):
+        connection = self.connect()
+        cursor = connection.cursor()
+        try:
+            sql = "INSERT INTO 'keywords' ('word', 'priority', 'idinformation') VALUES (%s, %s, %s)"
+            cursor.execute(sql, (word, priority, idinformation))
+            connection.commit()
+        finally:
+            connection.close()
+
+    def add_information(self, idinformation, piazzaid):
+        connection = self.connect()
+        cursor = connection.cursor()
+        try:
+            sql = "INSERT INTO 'information' ('idinformation', 'piazzaid) VALUES (%s, %s)"
+            cursor.execute(sql, (idinformation, piazzaid))
+            connection.commit()
+        finally:
+            connection.close()
+
+    def get_all_keywords(self):
+        connection = self.connect()
+        cursor = connection.cursor()
+        try:
+            sql = "SELECT '*' FROM 'keywords'"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
+        finally:
+            connection.close()
