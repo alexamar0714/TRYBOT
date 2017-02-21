481656f62b6b45359a932fb0b8da5df9c1f36374
DSandberg93 <Danisan@stud.ntnu.no>
2017-02-21 13:08:38 +0100

    Update Mint.py

--- Mint.py
+++ Mint.py
@@ -2,42 +2,95 @@ import pymysql.cursors
 import pymysql
 
 
-class Mint():
+class Mint:
 
-    def connect(self, host="localhost", user="root", password="root", db="trybot"):
+    # connect-function connects ty the desired database
+    def connect(self, host=str, user=str, password=str, db=str):
+        '''
+
+        Connects to the desired database
+
+        :type host: str
+        :param host: Host name/IP-address
+
+        :type user: str
+        :param user: User used to connect to the database
+
+        :type password: str
+        :param password: Users password
+
+        :type db: str
+        :param db: The database to connect to
+
+        :return: Returns the connection
+        '''
+
         connection = pymysql.connect(host=host, user=user, password=password, db=db, charset='utf8')
         return connection
 
     def add_keyword(self, word=str, priority=str, idinformation=str):
-        connection = self.connect()
-        cursor = connection.cursor()
-        try:
-            sql = "INSERT INTO 'keywords' ('word', 'priority', 'idinformation') VALUES (%s, %s, %s)"
-            cursor.execute(sql, (word, priority, idinformation))
-            connection.commit()
-        finally:
-            connection.close()
+        '''
 
-    def add_information(self, idinformation, piazzaid):
-        connection = self.connect()
+        Adds a keyword to the keyword table in the database
+
+        :type word: str
+        :param word: The keyword to add
+
+        :type priority: str
+        :param priority: The keyword's priority
+
+        :type idinformation: str
+        :param idinformation: The keyword's foreign key to the information table
+
+        '''
+        connection = self.connect()  # Sets up a connection to the database
         cursor = connection.cursor()
         try:
-            sql = "INSERT INTO 'information' ('idinformation', 'piazzaid) VALUES (%s, %s)"
-            cursor.execute(sql, (idinformation, piazzaid))
-            connection.commit()
+            sql = "INSERT INTO keywords (word, priority, idinformation) VALUES (%s, %s, %s)"  # Query to be performed
+            cursor.execute(sql, (word, priority, idinformation))  # Executes the query
+            connection.commit()  # Commits the execution
         finally:
-            connection.close()
+            connection.close()  # Closes the connection to the database
+
+    def add_information(self, idinformation=str, piazzaid=str):
+        '''
+
+        Adds information to the information table in the database
+
+        :type idinformation: str
+        :param idinformation: The information id number
+
+        :type piazzaid: str
+        :param piazzaid: The id of the post on Piazza
+
+        '''
+        connection = self.connect()  # Sets up a connection to the database
+        cursor = connection.cursor()
+        try:
+            sql = "INSERT INTO information (idinformation, piazzaid) VALUES (%s, %s)"  # The query to be executed
+            cursor.execute(sql, (idinformation, piazzaid))  # Executes the query
+            connection.commit()  # Commits the execution
+        finally:
+            connection.close()  # Closes the connection to the database
 
     def get_all_keywords(self):
-        connection = self.connect()
+        '''
+
+        Gets all the keywords with all their data
+
+        :return: Returns a list of tuples with all keywords and their data
+
+        '''
+
+        connection = self.connect()  # Sets up a connection
         cursor = connection.cursor()
         try:
-            sql = "SELECT * FROM keywords"
-            cursor.execute(sql)
+            sql = "SELECT * FROM keywords"  # The query to be executed
+            cursor.execute(sql)  # Executes the query
             result = cursor.fetchall()
-            return result
+            return result  # Returns the result as a list of tuples
         finally:
-            connection.close()
+            connection.close()  # Closes the connection to the database
      
     def get_all_information(self):
         connection = self.connect()
