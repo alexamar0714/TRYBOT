6ce47b3335fe7b4916acddbcb1daf50951c93143
mrCardizzle <shivam-verma2006@hotmail.com>
2017-02-23 09:09:10 +0100

    Update TestMint.py

--- TestMint.py
+++ TestMint.py
@@ -12,6 +12,29 @@ class TestMint(unittest.TestCase):
     user = 'root'
     password = 'root'
     db = 'trybot'
+    
+    alleKeywords = ((hei,5,2),(melk,1,2),(ole,1,2),(melk,7,3),(ole,1,4),(petter,1,3),(kopmis,20,50),(melk,1,8))
+    alleInformation = ((2,4923),(3,9704),(4,8923),(5,5003),(6,7483))
+
+    def initValues():
+        mint = Mint()
+        mint.connect(host,user,password,db)
+        mint.addKeyWord("hei","5","2")
+        mint.addKeyWord("melk","1","2")
+        mint.addKeyWord("ole","1","2")
+        mint.addKeyWord("melk","7","3")
+        mint.addKeyWord("ole","1","4")
+        mint.addKeyWord("petter","1","3")
+        mint.addKeyWord("kompis","20","5")
+        mint.addKeyWord("melk","1","8")
+
+        mint.addInformation("2","4923")
+        mint.addInformation("3","9704")
+        mint.addInformation("4","8923")
+        mint.addInformation("5","5003")
+        mint.addInformation("6","7483")
+
+        mint.close()
 
     def connection_test(self):
         mint = Mint()
