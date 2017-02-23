26ce07a8d20760e4d83c1b4e5b43d3d23596eabc
mrCardizzle <shivam-verma2006@hotmail.com>
2017-02-23 10:51:08 +0100

    Update Mint.py

--- Mint.py
+++ Mint.py
@@ -3,25 +3,19 @@ import pymysql
 
 
 class Mint:
+
     def connect(self, host='localhost', user='root', password='root', db='trybot'):
         '''
-
         Connects to the desired database
-
         :type host: str
         :param host: Host name/IP-address
-
         :type user: str
         :param user: User used to connect to the database
-
         :type password: str
         :param password: Users password
-
         :type db: str
         :param db: The database to connect to
-
         :return: Connection
-
         '''
 
         try:
@@ -33,18 +27,13 @@ class Mint:
 
     def add_keyword(self, word=str, priority=str, idinformation=str):
         '''
-
         Adds a keyword to the keyword table in the database
-
         :type word: str
         :param word: The keyword to add
-
         :type priority: str
         :param priority: The keyword's priority
-
         :type idinformation: str
         :param idinformation: The keyword's foreign key to the information table
-
         '''
         connection = self.connect()  # Sets up a connection to the database
         cursor = connection.cursor()
@@ -60,15 +49,11 @@ class Mint:
 
     def add_information(self, idinformation=str, piazzaid=str):
         '''
-
         Adds information to the information table in the database
-
         :type idinformation: str
         :param idinformation: The information id number
-
         :type piazzaid: str
         :param piazzaid: The id of the post on Piazza
-
         '''
         connection = self.connect()  # Sets up a connection to the database
         cursor = connection.cursor()
@@ -84,17 +69,14 @@ class Mint:
 
     def get_all_keywords(self):
         '''
-
         Gets all rows of the keywords table
-
         :return: list of tuples
-
         '''
 
         connection = self.connect()  # Sets up a connection
         cursor = connection.cursor()
         try:
-            sql = "SELECT word, priority, idinformation FROM keywords"  # Query
+            sql = "SELECT word,priority, idinformation FROM keywords"  # Query
             cursor.execute(sql)  # Executes the query
             result = cursor.fetchall()
             return result  # Returns the result as a list of tuples if successful
@@ -105,11 +87,8 @@ class Mint:
 
     def get_all_information(self):
         '''
-
         Gets all the rows of the information table
-
         :return: List of tuples
-
         '''
 
         connection = self.connect()  # Sets up a connection to the database
@@ -126,18 +105,15 @@ class Mint:
 
     def get_keywords_with_information(self, piazza_id=str):
         '''
-
         Gets all the rows in the keywords table with a specific information id
-
         :type piazza_id: str
         :param piazza_id: The piazza id of the wanted keywords
-
         :return: List of tuples
         '''
         connection = self.connect()  # Sets up a connection to the database
         cursor = connection.cursor()
         try:
-            sql = "SELECT keywords.* FROM keywords INNER JOIN information ON keywords.idinformation = information.idinformation WHERE piazzaid = " + piazza_id  # Query
+            sql = "SELECT keywords.word, keywords.priority, ketwords.idinformation FROM keywords INNER JOIN information ON keywords.idinformation = information.idinformation WHERE piazzaid = " + piazza_id  # Query
             cursor.execute(sql)  # Executes the query
             result = cursor.fetchall()
             return result  # Returns a list of tuples with the wanted rows in the keyword table if successful
@@ -148,18 +124,15 @@ class Mint:
 
     def get_keyword_with_id(self, index_id=str):
         '''
-
         Gets the row in the keyword table with specific id
-
         :type index_id: str
         :param index_id: The id of the wanted keyword
-
         :return: Tuple
         '''
-        connection = self.connect()  # Sets up a connection to the database
+        connection = self.connect()    # Sets up a connection to the database
         cursor = connection.cursor()
         try:
-            sql = "SELECT keywords.* FROM keywords WHERE keywords.idkeywords =" + index_id  # Query
+            sql = "SELECT keywords.word, keywords.priority, ketwords.idinformation FROM keywords WHERE keywords.idkeywords =" + index_id  # Query
             cursor.execute(sql)  # Executes the query
             result = cursor.fetchall()
             return result  # Returns the wanted row in the keyword table as a tuple if successful
@@ -167,27 +140,24 @@ class Mint:
             return False  # Returns False if getting failed
         finally:
             connection.close()  # Closes the connection to the database
-
+            
     def get_highest_pri(self, soke_liste):
         '''
-
-        Gets the piazzaIDs and sum of priority matching the words given in the array as input to the function
-
-        :type soke_liste: str array
+        Gets the piazzaIDs and sum of priority matching the words given in the array as input to the function 
+        :type soke_liste: str array 
         :param soke_liste: eks ["sokeord1","sokeord2","sokeord3"]
-
         :return: List of tuples matching the input with desc sumpri, ((sumpri1,piazzaid1),(sumpri2,piazzaid2)): ((7,5738),(5,3245),(3,6578))
         '''
-
+        
         connection = self.connect()
         cursor = connection.cursor()
         try:
-            soke_string = ""  # Makes an empty string to put in the sql statement
-            for word in soke_liste:  # Builds the string to filter out words in the sql statement based on the input array
-                soke_string += "word = '" + word + "' OR "
-            soke_string = soke_string[:-3]  # Removes the last OR
-            # Joins the two tables, sum the prioreties, groups by the informationid and filters out the words.
-            sql = "SELECT information.piazzaid, CAST(SUM(priority) AS UNSIGNED) AS sumpri FROM keywords INNER JOIN information ON keywords.idinformation = information.idinformation WHERE " + soke_string + " GROUP BY information.idinformation ORDER BY sumpri DESC LIMIT 3"
+            soke_string = "" #Makes an empty string to put in the sql statement 
+            for word in soke_liste: #Builds the string to filter out words in the sql statement based on the input array
+                soke_string+="word = '"+word+"' OR "
+            soke_string = soke_string[:-3] #Removes the last OR
+            #Joins the two tables, sum the prioreties, groups by the informationid and filters out the words.
+            sql = "SELECT information.piazzaid, CAST(SUM(priority) AS UNSIGNED) AS sumpri FROM keywords INNER JOIN information ON keywords.idinformation = information.idinformation WHERE "+soke_string+" GROUP BY information.idinformation ORDER BY sumpri DESC LIMIT 3"
             cursor.execute(sql)
             result = cursor.fetchall()
             return result  # Returns result if successful
